1f8b6a796e4497a1d3d9715bd354faff1d26cc6d
ccsu <ccsu@stust.edu.tw>
2018-03-02 14:04:46 +0800

    3 Add Button

--- app/src/main/java/com/example/cc/test2/Main2Activity.kt
+++ app/src/main/java/com/example/cc/test2/Main2Activity.kt
@@ -2,11 +2,18 @@ package com.example.cc.test2
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        var b = findViewById<Button>(R.id.button)
+
+        b.setOnClickListener(){
+            b.setText("Hi")
+        }
     }
 }
